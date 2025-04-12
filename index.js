5df7a4b0959e87f49ff29a856933380666e2ebad
Henry1610 <trannguyentruong6@gmail.com>
2025-04-12 18:36:35 +0700

    update login register

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 const connectDB = require('./db/connect');
 
@@ -11,6 +12,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 connectDB(); // kết nối MongoDB
 
+// Middleware
+app.use(cors()); // Cho phép tất cả các origin
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
